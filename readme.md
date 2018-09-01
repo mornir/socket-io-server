2defee59d7c89d6ee276e11be60a5b9bcb4f750f
mornir <jeromepott@hotmail.com>
2018-09-01 10:17:48 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Websockets for Speak
+
+## Commands npm
+
+```sh
+npm start
+
+# With hot-reload (requires nodemon to be installed globally)
+# npm install -g nodemon
+npm run dev
+```
---
+++ server.js
@@ -0,0 +1,35 @@
+const socketIO = require('socket.io')
+const io = socketIO.listen(3000)
+
+io.on('connection', socket => {
+  console.log('user connected')
+
+  socket.on('join', room => {
+    socket.join(room)
+
+    socket.emit('welcome', 'welcome man')
+    socket.broadcast.to(room).emit('welcome', 'new user')
+  })
+
+  socket.on('player1Answer', answer => {
+    console.log('got answer', answer)
+    socket.broadcast.emit('player1AnswerReceived', answer)
+  })
+
+  // used in games 2 & 3
+  socket.on('rateAnswer', rating => {
+    console.log('got rating', rating)
+    io.to(rating.room).emit('ratedAnswer', rating)
+  })
+
+  // used in game 2
+  socket.on('selectCard', payload => {
+    console.log('got payload', payload)
+    io.to(payload.room).emit('selectedCard', payload)
+  })
+
+  socket.on('switchRole', room => {
+    console.log('switchRole')
+    io.to(room).emit('switchedRole')
+  })
+})
